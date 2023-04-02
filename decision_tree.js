352afd9c0251f56e72099942a9e95e0c3bf47ca7
Quentin Vincenot <quentin.vincenot@gmail.com>
2023-04-02 15:08:53 +0200

    Tried out few visualizations for tree page

--- decision_tree.js
+++ decision_tree.js
@@ -18,250 +18,82 @@ var virginica_datapoints = [
     {"sepalLength": 7.1, "sepalWidth": 3.0, "petalLength": 5.9, "petalWidth": 2.1, "species": "virginica"}];
 
 //----------------------------------------
-//--- Donut
+//--- Bubble chart
 //----------------------------------------
 
-var iris_donut = new Chart(document.getElementById('iris_donut').getContext('2d'), {
-    type: 'doughnut',
+var bubble_chart = new Chart(document.getElementById('bubble_chart').getContext('2d'), {
+    type: 'bubble',
     data: {
-        labels: ['Setosa', 'Versicolor', 'Virginica'],
         datasets: [{
-            label: 'Iris amount',
-            data: [setosa_datapoints.length, versicolor_datapoints.length, virginica_datapoints.length],
-            backgroundColor: ['rgba(0, 190, 0, 0.4)', 'rgba(50, 0, 255, 0.4)', 'rgba(235, 80, 80, 0.4)'],
-            borderColor: ['rgba(0, 190, 0, 0.8)', 'rgba(50, 0, 255, 0.8)', 'rgba(235, 80, 80, 0.8)'],
-            borderWidth: 1
+            label: 'Dataset 1', data: [{x:12, y:48, r:7}, {x:17, y:24, r:15}, {x:8, y:25, r:10}],
+            backgroundColor: 'rgba(255, 99, 132, 0.8)', borderColor: 'rgba(255, 99, 132, 1)'
+        }, {
+            label: 'Dataset 2', data: [{x:11, y:36, r:15}, {x:27, y:13, r:7}],
+            backgroundColor: 'rgba(54, 162, 235, 0.8)', borderColor: 'rgba(54, 162, 235, 1)'
+        }, {
+            label: 'Dataset 3', data: [{x:14, y:30, r:10}, {x:19, y:42, r:7}, {x:22, y:15, r:15}],
+            backgroundColor: 'rgba(75, 192, 192, 0.8)', borderColor: 'rgba(75, 192, 192, 1)'
         }]
     },
     options: {
         responsive: true, layout: {padding: 10},
-        cutout: '80%',
         plugins: {
-            title: {display: true, text: 'Iris sample distribution among classes'},
+            title: {display: true, text: 'Bubble chart'},
             legend: {position: 'top'}
         }
     }
 });
 
-
 //----------------------------------------
-//--- Boxplot
+//--- Vertical barchart
 //----------------------------------------
 
-// Prepare datapoints from Iris Setosa (sepals/petals length/width) into appropriate boxplot objects
-var setosa_sepal_lengths = []; var setosa_sepal_widths = [];
-var setosa_petal_lengths = []; var setosa_petal_widths = [];
-for(var i=0; i<setosa_datapoints.length; i++) {
-    setosa_sepal_lengths.push(setosa_datapoints[i].sepalLength);
-    setosa_sepal_widths.push(setosa_datapoints[i].sepalWidth);
-    setosa_petal_lengths.push(setosa_datapoints[i].petalLength);
-    setosa_petal_widths.push(setosa_datapoints[i].petalWidth);
-}
-
-// Prepare datapoints from Iris Versicolor (sepals/petals length/width) into appropriate boxplot objects
-var versicolor_sepal_lengths = []; var versicolor_sepal_widths = [];
-var versicolor_petal_lengths = []; var versicolor_petal_widths = [];
-for(var i=0; i<versicolor_datapoints.length; i++) {
-    versicolor_sepal_lengths.push(versicolor_datapoints[i].sepalLength);
-    versicolor_sepal_widths.push(versicolor_datapoints[i].sepalWidth);
-    versicolor_petal_lengths.push(versicolor_datapoints[i].petalLength);
-    versicolor_petal_widths.push(versicolor_datapoints[i].petalWidth);
-}
-
-// Prepare datapoints from Iris Virginica (sepals/petals length/width) into appropriate boxplot objects
-var virginica_sepal_lengths = []; var virginica_sepal_widths = [];
-var virginica_petal_lengths = []; var virginica_petal_widths = [];
-for(var i=0; i<virginica_datapoints.length; i++) {
-    virginica_sepal_lengths.push(virginica_datapoints[i].sepalLength);
-    virginica_sepal_widths.push(virginica_datapoints[i].sepalWidth);
-    virginica_petal_lengths.push(virginica_datapoints[i].petalLength);
-    virginica_petal_widths.push(virginica_datapoints[i].petalWidth);
-}
-
-var iris_features_boxplot = new Chart(document.getElementById('iris_features_boxplot').getContext('2d'), {
-    type: 'boxplot',
+var vertical_barchart = new Chart(document.getElementById('vertical_barchart').getContext('2d'), {
+    type: 'bar',
     data: {
-        labels: ['Sepal length', 'Sepal width', 'Petal length', 'Petal width'],
+        labels: ['A', 'B', 'C'],
         datasets: [{
-            label: 'Setosa',
-            data: [setosa_sepal_lengths, setosa_sepal_widths, setosa_petal_lengths, setosa_petal_widths],
-            backgroundColor: 'rgba(0, 190, 0, 0.4)', borderColor: 'rgba(0, 190, 0, 0.8)',
-            borderWidth: 1
-        },
-        {
-            label: 'Versicolor',
-            data: [versicolor_sepal_lengths, versicolor_sepal_widths, versicolor_petal_lengths, versicolor_petal_widths],
-            backgroundColor: 'rgba(50, 0, 255, 0.4)', borderColor: 'rgba(50, 0, 255, 0.8)',
-            borderWidth: 1
-        },
-        {
-            label: 'Virginica',
-            data: [virginica_sepal_lengths, virginica_sepal_widths, virginica_petal_lengths, virginica_petal_widths],
-            backgroundColor: 'rgba(235, 80, 80, 0.4)', borderColor: 'rgba(235, 80, 80, 0.8)',
-            borderWidth: 1
+            label: 'Dataset 1', data: [12, 34, 27],
+            backgroundColor: 'rgba(255, 99, 132, 0.8)', borderColor: 'rgba(255, 99, 132, 1)'
+        }, {
+            label: 'Dataset 2', data: [15, 30, 28],
+            backgroundColor: 'rgba(54, 162, 235, 0.8)', borderColor: 'rgba(54, 162, 235, 1)'
+        }, {
+            label: 'Dataset 3', data: [15, 30, 28],
+            backgroundColor: 'rgba(75, 192, 192, 0.8)', borderColor: 'rgba(75, 192, 192, 1)'
         }]
     },
     options: {
         responsive: true, layout: {padding: 10},
-        maintainAspectRatio: false,
         indexAxis: 'y',
-        scales: {y: {beginAtZero: true}},
+        elements: {bar: {borderWidth: 1}},            
         plugins: {
-            title: {display: true, text: 'Features distributions of Iris classes'},
+            title: {display: true, text: 'Vertical barchart'},
             legend: {position: 'top'}
         }
     }
 });
 
+//----------------------------------------
+//--- Pie chart
+//----------------------------------------
 
-//--------------------------------------------------------------------------------
-//--- Scatterplot
-//--------------------------------------------------------------------------------
-
-// Prepare datapoints from Iris Setosa (petals length/width) into appropriate scatterplot objects
-var setosa_scatterplot_dataset = [];
-for(var i=0; i<setosa_datapoints.length; i++) {
-    var setosa_temp_object = {x: setosa_datapoints[i].petalLength, y: setosa_datapoints[i].petalWidth};
-    setosa_scatterplot_dataset.push(setosa_temp_object);
-}
-
-// Prepare datapoints from Iris Versicolor (petals length/width) into appropriate scatterplot objects
-var versicolor_scatterplot_dataset = [];
-for(var i=0; i<versicolor_datapoints.length; i++) {
-    var versicolor_temp_object = {x: versicolor_datapoints[i].petalLength, y: versicolor_datapoints[i].petalWidth};
-    versicolor_scatterplot_dataset.push(versicolor_temp_object);
-}
-
-// Prepare datapoints from Iris Virginica (petals length/width) into appropriate scatterplot objects
-var virginica_scatterplot_dataset = [];
-for(var i=0; i<virginica_datapoints.length; i++) {
-    var virginica_temp_object = {x: virginica_datapoints[i].petalLength, y: virginica_datapoints[i].petalWidth};
-    virginica_scatterplot_dataset.push(virginica_temp_object);
-}
-
-var iris_petals_scatterplot = new Chart(document.getElementById('iris_petals_scatterplot').getContext('2d'), {
-    type: 'scatter',
+var pie_chart = new Chart(document.getElementById('pie_chart').getContext('2d'), {
+    type: 'pie',
     data: {
+        labels: ['A', 'B', 'C'],
         datasets: [{
-            label: 'Setosa', data: setosa_scatterplot_dataset,
-            pointRadius: 5, pointHoverRadius: 6, pointStyle: 'circle',
-            backgroundColor: 'rgba(0, 190, 0, 0.5)', pointHoverBackgroundColor: 'rgba(0, 190, 0, 0.7)',
-            borderColor: 'rgba(0, 190, 0, 0.8)'
-        }, {
-            label: 'Versicolor', data: versicolor_scatterplot_dataset,
-            pointRadius: 5, pointHoverRadius: 6, pointStyle: 'rectRot',
-            backgroundColor: 'rgba(50, 0, 255, 0.5)', pointHoverBackgroundColor: 'rgba(50, 0, 255, 0.7)',
-            borderColor: 'rgba(50, 0, 255, 0.8)'
-        }, {
-            label: 'Virginica', data: virginica_scatterplot_dataset,
-            pointRadius: 5, pointHoverRadius: 6, pointStyle: 'triangle',
-            backgroundColor: 'rgba(235, 80, 80, 0.5)', pointHoverBackgroundColor: 'rgba(235, 80, 80, 0.7)',
-            borderColor: 'rgba(235, 80, 80, 0.8)'
+            label: 'Dataset 1', data: [12, 34, 27],
+            backgroundColor: ['rgba(255, 99, 132, 0.8)', 'rgba(54, 162, 235, 0.8)', 'rgba(75, 192, 192, 0.8)'],
+            borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(75, 192, 192, 1)'],
+            borderWidth: 1
         }]
     },
     options: {
-        responsive: true, layout: {padding: 10},
-        maintainAspectRatio: false,
-        scales: {
-            x: { title: {display: true, text:'Petal length'}, min: 0.5, max: 7.0 },
-            y: { title: {display: true, text:'Petal width'}, min: 0.0, max: 3.0 }
-        },
+        responsive: true, layout: {padding: 10},       
         plugins: {
-            title: {display: true, text: 'Petal features distribution of Iris classes'},
+            title: {display: true, text: 'Pie chart'},
             legend: {position: 'top'}
-        }
-    }
-});
-
-
-//----------------------------------------
-//--- Radar chart
-//----------------------------------------
-
-// Prepare datapoints from Iris Setosa (petals length/width) into appropriate radar objects
-var setosa_radar_dataset = [0, 0, 0, 0];
-for(var i=0; i<setosa_datapoints.length; i++) {
-    setosa_radar_dataset[0] += setosa_datapoints[i].sepalLength;
-    setosa_radar_dataset[1] += setosa_datapoints[i].sepalWidth;
-    setosa_radar_dataset[2] += setosa_datapoints[i].petalLength;
-    setosa_radar_dataset[3] += setosa_datapoints[i].petalWidth;
-    if(i == setosa_datapoints.length - 1) {
-        setosa_radar_dataset[0] /= setosa_datapoints.length;
-        setosa_radar_dataset[1] /= setosa_datapoints.length;
-        setosa_radar_dataset[2] /= setosa_datapoints.length;
-        setosa_radar_dataset[3] /= setosa_datapoints.length;
-    }
-}
-
-// Prepare datapoints from Iris Versicolor (petals length/width) into appropriate radar objects
-var versicolor_radar_dataset = [0, 0, 0, 0];
-for(var i=0; i<versicolor_datapoints.length; i++) {
-    versicolor_radar_dataset[0] += versicolor_datapoints[i].sepalLength;
-    versicolor_radar_dataset[1] += versicolor_datapoints[i].sepalWidth;
-    versicolor_radar_dataset[2] += versicolor_datapoints[i].petalLength;
-    versicolor_radar_dataset[3] += versicolor_datapoints[i].petalWidth;
-    if(i == versicolor_datapoints.length - 1) {
-        versicolor_radar_dataset[0] /= versicolor_datapoints.length;
-        versicolor_radar_dataset[1] /= versicolor_datapoints.length;
-        versicolor_radar_dataset[2] /= versicolor_datapoints.length;
-        versicolor_radar_dataset[3] /= versicolor_datapoints.length;
-    }
-}
-
-// Prepare datapoints from Iris Virginica (petals length/width) into appropriate radar objects
-var virginica_radar_dataset = [0, 0, 0, 0];
-for(var i=0; i<virginica_datapoints.length; i++) {
-    virginica_radar_dataset[0] += virginica_datapoints[i].sepalLength;
-    virginica_radar_dataset[1] += virginica_datapoints[i].sepalWidth;
-    virginica_radar_dataset[2] += virginica_datapoints[i].petalLength;
-    virginica_radar_dataset[3] += virginica_datapoints[i].petalWidth;
-    if(i == virginica_datapoints.length - 1) {
-        virginica_radar_dataset[0] /= virginica_datapoints.length;
-        virginica_radar_dataset[1] /= virginica_datapoints.length;
-        virginica_radar_dataset[2] /= virginica_datapoints.length;
-        virginica_radar_dataset[3] /= virginica_datapoints.length;
-    }
-}
-
-// Compute the max span of the radar based on the Iris max features values
-var maxRadarValue = Math.max(Math.max(setosa_radar_dataset), Math.max(versicolor_radar_dataset), Math.max(virginica_radar_dataset)) + 0.2;
-
-var iris_features_radar = new Chart(document.getElementById('iris_features_radar').getContext('2d'), {
-    type: 'radar',
-    data: {
-        labels: ['Sepal length', 'Sepal width', 'Petal length', 'Petal width'],
-        datasets: [{
-            label: 'Setosa', data: setosa_radar_dataset,
-            fill: true, borderWidth: 1, pointRadius: 4, pointHoverRadius: 5,
-            backgroundColor: 'rgba(0, 190, 0, 0.5)', borderColor: 'rgba(0, 190, 0, 0.5)',
-            pointBorderColor: 'rgba(255, 255, 255, 0.7)', pointHoverBackgroundColor: 'rgba(0, 190, 0, 0.7)'
-        }, {
-            label: 'Versicolor', data: versicolor_radar_dataset,
-            fill: true, borderWidth: 1, pointRadius: 4, pointHoverRadius: 5,
-            backgroundColor: 'rgba(50, 0, 255, 0.5)', borderColor: 'rgba(50, 0, 255, 0.5)',
-            pointBorderColor: 'rgba(255, 255, 255, 0.7)', pointHoverBackgroundColor: 'rgba(50, 0, 255, 0.7)'
-        }, {
-            label: 'Virginica', data: virginica_radar_dataset,
-            fill: true, borderWidth: 1, pointRadius: 4, pointHoverRadius: 5,
-            backgroundColor: 'rgba(235, 80, 80, 0.5)', borderColor: 'rgba(235, 80, 80, 0.5)',
-            pointBorderColor: 'rgba(255, 255, 255, 0.7)', pointHoverBackgroundColor: 'rgba(235, 80, 80, 0.7)'
-        }]
-    },
-    options: {
-        responsive: true, layout: {padding: 10},
-        scales: {
-            r: {
-                suggestedMin: 0.0, suggestedMax: maxRadarValue,
-                ticks: {backdropColor: 'rgba(255, 255, 255, 0.0)'}
-            }
-        },
-        plugins: {
-            title: {display: true, text: 'Features mean values of Iris classes'},
-            filler: {propagate: false},
-            'samples-filler-analyser': {target: 'chart-analyser'}
-        },
-        interaction: {
-            intersect: false
         }
     }
 });
